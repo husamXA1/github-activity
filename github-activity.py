34d5ad3890ace294b0f3d29b53a17bd83090bd30
husamXA1 <husamxm0@gmail.com>
2024-12-10 11:11:52 +0200

    Refactored code to get_activities function

--- github-activity.py
+++ github-activity.py
@@ -1,27 +1,33 @@
 import sys, urllib.request, json
 
+def get_activities(username):
+	url = f"https://api.github.com/users/{username}/events"
+
+	with urllib.request.urlopen(url) as response:
+		data = json.loads(response.read().decode('utf-8'))
+
+	result = ""
+	if data:
+		for event in data:
+			result += f"[{event["created_at"]}] {event["repo"]["name"]}: "
+			match event["type"]:
+				case "CreateEvent":
+					result += f"Created {event["payload"]["ref_type"]} {event["payload"]["ref"] or ""}\n"
+				case "ForkEvent":
+					result += f"Forked on {event["payload"]["forkee"]["full_name"]}\n"
+				case "PushEvent":
+					result += f"Pushed {event["payload"]["size"]} commit{"s" if event["payload"]["size"] > 1 else ""} on "
+					if event["payload"]["ref"].split("/")[1] == "tags":
+						result += f"tag {event["payload"]["ref"].split("/")[-1]}\n"
+					else:
+						result += f"{event["payload"]["ref"].split("/")[-1]} branch\n"
+				case _:
+					result += f"{event["type"]}\n"
+	
+	return result.strip()
+
 if len(sys.argv) != 2:
 	print("USAGE: github-activity [USERNAME]")
 	sys.exit(1)
 
-url = f"https://api.github.com/users/{sys.argv[1]}/events"
-
-with urllib.request.urlopen(url) as response:
-    data = json.loads(response.read().decode('utf-8'))
-
-if data:
-	for event in data:
-		print(f"[{event["created_at"]}] {event["repo"]["name"]}", end=": ")
-		match event["type"]:
-			case "CreateEvent":
-				print(f"Created {event["payload"]["ref_type"]} {event["payload"]["ref"] or ""}")
-			case "ForkEvent":
-				print(f"Forked on {event["payload"]["forkee"]["full_name"]}")
-			case "PushEvent":
-				print(f"Pushed {event["payload"]["size"]} commit{"s" if event["payload"]["size"] > 1 else ""} on ", end="")
-				if event["payload"]["ref"].split("/")[1] == "tags":
-					print(f"tag {event["payload"]["ref"].split("/")[-1]}")
-				else:
-					print(f"{event["payload"]["ref"].split("/")[-1]} branch")
-			case _:
-				print(f"{event["type"]}")
+print(get_activities(sys.argv[1]))
